4b155fc2c256fe39650aed8a4af2ee6c79d54eee
kurishchev <kurishchev@rambler.ru>
2024-07-28 17:06:57 +0300

    Add files via upload

---
+++ namespaces.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print('Я')
+
+    print('inner_function внутри функции test_function, ау?')
+    inner_function()
+test_function()
+# print('inner_function вне функции test_function, ау?')   # -отсюда не вызывается
+# inner_function
